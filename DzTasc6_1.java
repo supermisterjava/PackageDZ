e0ff636cc43b6a2f4490b0aec1a367686f423819
supermisterjava <169040864+supermisterjava@users.noreply.github.com>
2025-09-03 13:04:16 +0500

    Add files via upload

---
+++ DzTasc6_1.java
@@ -0,0 +1,43 @@
+package dz;
+
+import java.util.Arrays;
+
+//Задача № 1
+//
+//Вы ведете записи наивысшей дневной температуры. Найдите самый жаркий день.
+// Найдите второй самый жаркий. Предполагается, что во все дни температура разная.
+public class DzTasc6_1 {
+    public static void main(String[] args) {
+
+        int[] temperatures = {32, 28, 35, 29, 31, 34, 30, 33, 27, 36};
+
+//        Initialize the two hottest days
+}
+
+//        int[] temperatures = {32, 28, 35, 29, 31, 34, 30, 33, 27, 36};
+//        int[] arraySorted = Arrays.copyOf(temperatures, temperatures.length);
+//        Arrays.sort(arraySorted);
+//
+//        System.out.println(Arrays.toString(arraySorted));
+////        System.out.println(Arrays.toString(arraySorted[arraySorted.length - 1]));
+//        int index = 0;
+//        for (int i = arraySorted.length; i > arraySorted.length-1; i--) {
+//            index = arraySorted[i];
+//        }
+//        System.out.println(index);
+//    }
+//}
+
+//        System.out.println(temperatures.length -1 );
+//        System.out.println(Arrays.toString(temperatures.length ) - 1);
+//        int theHottestDay = 0;
+//
+//        for (int i = 0; i < temperatures.length ;i++)
+//             if(  temperatures[i] > theHottestDay){
+//                 theHottestDay = temperatures[i];
+//
+//
+//        }
+//        System.out.println(theHottestDay);
+//    }
+//}
