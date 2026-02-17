31301b3bf855d31aa297195781cf8a79137599e2
Demo <169040864+supermisterjava@users.noreply.github.com>
2026-02-17 10:27:56 +0500

    Add files via upload

---
+++ Dz5_4.java
@@ -0,0 +1,29 @@
+package dz;
+
+import java.util.Arrays;
+
+//Задача №4
+//Дана строка:
+//String s = “Перестановочный алгоритм быстрого действия”;
+//необходимо вывести все буквы “о” из этой строки.
+//Для указанной строки ответ будет “ооооо” (или в столбик)
+public class Dz5_4 {
+    public static void main(String[] args) {
+        String s = "Перестановочный алгоритм быстрого действия";
+
+        System.out.println(s.indexOf('о'));
+//        s.toCharArray();
+//        String[] strings = {"Перестановочный алгоритм быстрого действия"};
+//        for (int i = 0; i < strings.length; i++) {
+//            if (strings[i] == "о") {
+//                System.out.println(strings[i]);
+//        System.out.println(s.Arrays.toString());
+//        for (int i = 0; i < s.length() ; i++) {
+//            if(s.[i] = "о"){
+//                System.out.println();
+//            }
+//        }
+//        System.out.println(Arrays.toString(s.split("о")));
+
+    }
+}
