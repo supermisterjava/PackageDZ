640a8c7ae2ba629f3d7c9a4529c0d3fe99390926
Demo <169040864+supermisterjava@users.noreply.github.com>
2026-03-25 16:21:56 +0500

    Add files via upload

---
+++ MySeleniumTest1.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class MySeleniumTest1 {
+    public void firstTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+
+        Assert.assertEquals(message.getText(), "Received!");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testGoogle() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        WebElement textInput = driver.findElement(By.id("APjFqb"));
+        textInput.sendKeys("selenium");
+
+        WebElement aiButton = driver.findElement(By.xpath("//button[@jsname='B6rgad']"));
+        aiButton.click();
+
+        WebElement 
+
+        driver.quit();
+    }
+
+}
+
+
